6db1824876ff4c50ea19b70a572d92e96bab6207
AgustinYpuntofinal <agusleondiazcoliqueo@gmail.com>
2025-09-19 15:17:41 -0300

    Add files via upload

---
+++ ejemplo1.js
@@ -0,0 +1,5 @@
+let parrafo= document.getElementById("texto");
+let button= document.getElementById("btn");
+button.addEventListener("click",function(){
+    parrafo.innerHTML="PERO QUE MINIATURA🗣️❗❗🔥";
+});
